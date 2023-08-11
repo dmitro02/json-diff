6bf36919309ee64a23f8054bff8d046ec1a9e942
dmitro02 <dmitro02@gmail.com>
2023-08-11 22:10:37 +0300

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,15 @@ import * as R from 'ramda';
 
 import { left, right } from './data';
 
+const styles = {
+  key: 'color: #0451a5',
+  string: 'color: #a31515',
+  number: 'color: #098658',
+  boolean: 'color: #0000ff',
+  null: 'color: #d900ff',
+  changed: 'background-color: #ffc628',
+}
+
 export default function App() {
   const rightWithDiff = diff(left, right);
 
@@ -37,25 +46,26 @@ function syntaxHighlight(json: string) {
   return json.replace(
     /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
     function (match) {
-      var cls = 'number';
+      var style = styles.number;
       if (/^"%%%/.test(match)) {
-        cls = 'changed';
+        style = styles.changed;
         match = match.replaceAll('%%%', '');
       } else if (/^"/.test(match)) {
         if (/:$/.test(match)) {
-          cls = 'key';
+          style = styles.key;
         } else {
-          cls = 'string';
+          // cls = 'string';
+          style = styles.string;
         }
       } else if (/true|false/.test(match)) {
-        cls = 'boolean';
+        style = styles.boolean;
       } else if (/null/.test(match)) {
-        cls = 'null';
+        style = styles.null;
       }
 
       match = handleQuotes(match);
 
-      return '<span class="' + cls + '">' + match + '</span>';
+      return '<span style="' + style + '">' + match + '</span>';
     }
   );
 }
